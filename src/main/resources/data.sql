501b4362b4bca36268f60e4e74e239bd5e34c945
Saan Saechao <saan@saechao.fyi>
2025-05-13 14:32:45 -0700

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
+values(10001,'USD','JPY',147);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
+values(10002,'USD','EUR',0.91);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
+values(10003,'USD','AUD',1.65);
